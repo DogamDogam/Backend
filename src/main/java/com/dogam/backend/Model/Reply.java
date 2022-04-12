f8c674cb33f46680177ffefa96fabddb8e6ebc6e
dhgkdud12 <dhgkdud14@gmail.com>
2022-04-12 21:43:48 +0900

    MySQL 연동, CASCADE 추가

--- src/main/java/com/dogam/backend/Model/Reply.java
+++ src/main/java/com/dogam/backend/Model/Reply.java
@@ -1,6 +1,8 @@
 package com.dogam.backend.Model;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 import javax.persistence.*;
 import java.sql.Timestamp;
@@ -25,6 +27,7 @@ public class Reply {
 
     @ManyToOne //여러개의 답변은 하나의 게시물에 존재
     @JoinColumn(name="postId")
+    @OnDelete(action = OnDeleteAction.CASCADE)
     private Post post;
 
     @CreationTimestamp
